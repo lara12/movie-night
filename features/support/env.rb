60bf6718a418421860f1e70f16b13105ee4effad
Aliaksei Piatrouski <Aliaksei@Piatrouski>
2015-11-02 17:05:30 -0800

    initial commit

---
+++ features/support/env.rb
@@ -0,0 +1,29 @@
+require 'cucumber'
+require 'appium_lib'
+require 'rspec'
+require 'pry'
+
+
+APP_PATH = ENV['APP'] ||  File.join(File.dirname(__FILE__), "..", "..", "Movies.app")
+DEFAULT_TIMEOUT = 20
+
+def caps
+  { caps:       { deviceName: (ENV['DEVICE'] || "iPhone 6"),
+                  platformName: 'iOS',
+                  platformVersion: (ENV['SDK'] || '8.4'),
+                  app: APP_PATH,
+                  bundleId: 'de.tum.in.www1.ios15.Movies',
+                  locationServicesEnabled: true,
+                  locationServicesAuthorized: true,
+                  newCommandTimeout: '36000'},
+    appium_lib: { wait: DEFAULT_TIMEOUT,
+                  debug: false } }
+end
+binding.pry
+# Appium::Driver.new(caps)
+# Appium.promote_appium_methods ScreenActions
+
+World do
+  # Wikipedia.new
+end
+
